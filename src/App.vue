9dcd1091904d6e8faee91cff6ecdcf18e3d17965
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-16 18:17:21 +0200

    aggiungo chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script >
-export default{
+import axios from "axios";
+export default {
+  data() {
+    return {
+      projects: [],
+      pagination: {}
 
-}
+    }
+  },
+
+  methods: {
+    fetchData() {
+      axios.get("http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          // salvo i progetti 
+          this.projects = response.data.results.data;
+
+          // salvo i dati relativi alla paginazione con il delete cancello la chiave data 
+          delete response.data.results.data;
+          
+          this.pagination = response.data.results;
+          console.log(response);
+        })
+      }
+    
+  },
+  
+  mounted(){
+    this.fetchData();
+  }
+};
 </script>
 
 <template>
- <h1>ciao</h1>
+  <h1>ciao</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
