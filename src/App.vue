62bb014d2754a5ca4028f2e03c4c67ba9b94366b
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-16 18:42:14 +0200

    stampato in pagina progetti

--- src/App.vue
+++ src/App.vue
@@ -18,22 +18,38 @@ export default {
 
           // salvo i dati relativi alla paginazione con il delete cancello la chiave data 
           delete response.data.results.data;
-          
+
           this.pagination = response.data.results;
           console.log(response);
         })
-      }
-    
+    },
+    getImgUrl(project) {
+      return `http://127.0.0.1:8000/storage/${project.image}`;
+    },
   },
-  
-  mounted(){
+
+  mounted() {
     this.fetchData();
   }
 };
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <main>
+    <div class="container">
+
+      <div v-for="project in projects" :key="project.id" class="card-container">
+        <div class="card">
+          <div class="img-card">
+            <img :src="getImgUrl(project)" alt="">
+          </div>
+          <p>{{ project.title }}</p>
+          <p>{{ project.description }}</p>
+        </div>
+      </div>
+    </div>
+
+  </main>
 </template>
 
 <style scoped></style>
