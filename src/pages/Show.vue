4d2889eb0c7196cde2aa4aa8c9ebedebc0f1e7b7
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-10-17 19:28:09 +0200

    stampo in pagina singolo elemento in file show

--- src/pages/Show.vue
+++ src/pages/Show.vue
@@ -22,8 +22,10 @@ export default {
           console.log(this.project);
         })
     },
-
+    getImgUrl(project) {
+      return `http://127.0.0.1:8000/storage/${project.image}`;
   },
+},
 
   mounted() {
     this.fetchData();
@@ -33,7 +35,22 @@ export default {
 
 <template>
 
-    <h1>{{this.project.title}}</h1>
+<div class="card">
+          <div class="img-card">
+            <img :src="getImgUrl(project)" alt="">
+          </div>
+          <p>{{project.title }}</p>
+          <p>{{ project.description }}</p>
+          <a :href="project.github_link">Github link</a>
+         
+         
+         
+          <div class="tecnologie" v-for=" technology in project.technologies">
+           <p>{{technology.name }}</p>
+          </div>
+        
+          <p>{{project.type ? project.type.type: ""  }}</p>
+        </div>
 </template>
 
 <style scoped></style>
